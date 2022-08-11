db12d995a044207180962e134269679d350b3ea9
rajvir234567 <73382875+rajvir234567@users.noreply.github.com>
2022-08-11 16:25:16 +0100

    Add files via upload

--- project.js
+++ project.js
@@ -14,10 +14,27 @@ AFRAME.registerComponent("project",{
                 var cam = document.querySelector("#camera").object3D
                 var direction = new THREE.Vector3()
                 cam.getWorldDirection(direction)
-                ball.setAttribute("velocity", direction.multiplyScalar(-10))
+                ball.setAttribute("velocity", direction.multiplyScalar(-10));
+                ball.setAttribute("dynamic-body", {shape: "sphere", mass: "0"})
+                ball.addEventListener("collide", this.removeBall)
                 var scene = document.querySelector("#scene")
                 scene.appendChild(ball)
             }
         })
+    },
+    removeBall: function (e) {
+        console.log(e.detail.target.el);
+        console.log(e.detail.body.el);
+        var ball_el = e.detail.target.el
+        var elementHit = e.detail.body.el
+        if (elementHit.id.includes("pin")) {
+          elementHit.setAttribute("material", {opacity: 0.6, transparent: true})
+          var impulse = new CANNON.Vec3(-2, 2, 1)
+          var point = new CANNON.Vec3().copy(elementHit.getAttribute("position"))
+          elementHit.body.applyImpulse(impulse, point)
+          ball_el.removeEventListener("collide", this.shoot)
+          var scene = document.querySelector("#scene");
+          scene.removeChild(ball_el);
+      }
     }
 })
